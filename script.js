b0aaf0573696337f199e8c4ed8e6ccc01d1af047
wtg378900 <56295698+wtg378900@users.noreply.github.com>
2020-04-21 19:06:08 +0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+// an oreo cookie weighs 14.5 grams
+let oreo = 14.5;
+
+// one pound is equal to 454 grams
+let gramsPerPound = 500;
+
+let studentWeight;
+
+$("button").click(function() {
+    studentWeight = $("input").val();
+    let oreos = ( studentWeight * gramsPerPound ) / oreo;   
+    if(oreos>100){
+        alert("该运动了");  
+    }
+    
+    $("img").attr("src", "https://media.giphy.com/media/ibZqC0rHHnWIo/giphy.gif");
+    $("#oreoWeight").text("你的奥利奥体重是 " + oreos + " 奥利奥!");
+});
+
+
+
